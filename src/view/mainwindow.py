6170cdd86f2be9b96fe0638ab2f2a547e220d354
Alessa Lara <98236508+alessa-lara@users.noreply.github.com>
2026-09-17 16:21:02 -0300

    [partial] mainwindow bind functions to boxes and buttons

--- src/view/mainwindow.py
+++ src/view/mainwindow.py
@@ -5,12 +5,30 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.button_send.clicked.connect(self.send)
+        self.button_attach.clicked.connect(self.attach)
 
-    def get_tags(self):
-        pass
+    # probably can do something like:
+    # write a "," we create a button with the tag as a label and, when we click on the button, we can modify its text
+    def get_tags(self) -> list[str]:
+        tags_post: list[str] = self.input_tags.toPlainText().strip().split(",")
+        return tags_post
 
-    def get_post_text(self):
-        pass
+    def get_post_text(self) -> str:
+        post_text: str = self.input_post.toMarkdown()
+        return post_text
 
-    def send(self):
+    # the simplest way to attach a file to a post is saving the file location and then, later, showing the file
+    # not the most interesting or permanent way... but it works
+    def attach(self) -> str:
+        dialog = QtWidgets.QFileDialog()
+        file: tuple[str, str] = dialog.getOpenFileName(
+            self,
+            caption = "Select File",
+            directory = "/home",
+        )
+
+        return file[0]
+
+    def send(self) -> None:
         pass
